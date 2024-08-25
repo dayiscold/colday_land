5c9aa5c2d462413f474cfe6299b7075c753029a4
dayiscold <132763864+dayiscold@users.noreply.github.com>
2024-08-25 20:04:17 +0500

    №3 Список фото GET (#9)
    * Добавил таблицу релизов и их возвращение
    
    * Добавил метод POST для загрузки релизов
    
    * FIX Добавил метод POST для загрузки релизов
    
    * Добавлена функция удаления фото релиза
    
    * Добавлена функция возврата фото по айди
    
    * fix: photos
    
    ---------
    
    Co-authored-by: gubanov_d <3066897-gabenD@users.noreply.gitlab.com>

--- application/crud.py
+++ application/crud.py
@@ -1,13 +1,14 @@
+import base64
 from datetime import datetime
 from http import HTTPStatus
-from typing import List
+from typing import List, Iterator
 
-from fastapi import HTTPException
+from fastapi import HTTPException, status, UploadFile
 from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 from sqlalchemy.orm import Session
 
-from database import SiteInfo, SessionLocal
+from database import SiteInfo, SessionLocal, ReleasesInfo, PhotoFileReleases
 
 security = OAuth2PasswordBearer(tokenUrl="token")
 
@@ -43,6 +44,32 @@ class SiteInfoEdit(BaseModel):
     message: str
 
 
+class ReleasesSchemaItem(BaseModel):
+    id: int
+    created_at: datetime
+    updated_at: datetime | None
+    description: str | None
+    file_id: str
+    link: str
+
+
+class ReleasesSchema(BaseModel):
+    releases: list[ReleasesSchemaItem] = []
+
+
+class PhotoAddSchema(BaseModel):
+    id: int
+    filename: str
+
+
+class ReturnPhotoFromId(BaseModel):
+    id: int | None
+
+
+def get_current_user(session: Session):
+    pass
+
+
 def get_site_info(session: Session) -> SiteInfoSchema:
     site_info = session.query(SiteInfo).first()
     if not site_info:
@@ -52,6 +79,26 @@ def get_site_info(session: Session) -> SiteInfoSchema:
         description=site_info.description,
         links=SiteInfoLinksSchema.model_validate(site_info.links).links,
     )
+
+
+def get_photo_releases_list(releases: str | None = None, session=Session) -> ReleasesSchema:
+    response = ReleasesSchema()
+    if releases == "all":
+        rows = session.query(ReleasesInfo).all()
+    else:
+        rows = session.query(ReleasesInfo).order_by(ReleasesInfo.created_at.desc()).limit(3).all()
+    for release in rows:
+        response.releases.append(
+            ReleasesSchemaItem(
+                id=release.id,
+                created_at=release.created_at,
+                updated_at=release.updated_at,
+                description=release.description,
+                file_id=release.file_id,
+                link=release.link,
+            )
+        )
+    return response
 
 
 def edit_site_info(
@@ -69,3 +116,29 @@ def edit_site_info(
             )
         )
         session.commit()
+
+
+def append_photo(file: UploadFile, session: Session) -> int:
+    encoded_string = base64.b64encode(file.file.read()).decode("utf-8")
+    new_photo = PhotoFileReleases(filename=file.filename, content=encoded_string)
+    session.add(new_photo)
+    session.commit()
+    return new_photo.id
+
+
+def delete_photo(photo_id: ReturnPhotoFromId, session: Session) -> int:
+    if photo_id.id is None:
+        raise HTTPException(status_code=status.HTTP_500_INTERNAL_SERVER_ERROR, detail="Не передан идентификатор")
+    photo = session.query(PhotoFileReleases).filter(PhotoFileReleases.id == photo_id.id).first()
+    if photo is None:
+        raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, detail="Фотография не найдена")
+    session.delete(photo)
+    session.commit()
+    return photo_id.id
+
+
+def return_photo_from_id(photo_id: int, session: Session) -> Iterator[bytes]:
+    photo: PhotoFileReleases | None = session.query(PhotoFileReleases).filter(PhotoFileReleases.id == photo_id).first()
+    if not photo:
+        raise HTTPException(status_code=404, detail="Фото не найдено")
+    yield base64.b64decode(photo.content.encode("utf-8"))
